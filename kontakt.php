d6af14738cac88f30afcbef86aed0462343a1d6c
olijaun <olijaun@yahoo.com>
2017-12-27 18:33:44 +0100

    änderungen von alfredo

--- kontakt.php
+++ kontakt.php
@@ -1,66 +1,6 @@
 <?php include('include/header.html'); ?>
-
 <?php include('include/navigation.php'); ?>
-<div class="py-5 kontaktbg" style="height:100%">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-lg-10 col-lg-offset-1" data-form-type="formoid">
-
-            <h1>
-                Los Caracoles Kontakt Formular
-            </h1>
-            <p>
-            Bitte wenden Sie sich bei Kontaktaufnahme, für Mitteilungen, Empfehlungen und Vorschlägen an Los Caracoles wenn immer möglich per E-Mail. Danke.
-            </p>
-            <p<>
-            Allgemeines, Administration: <a href="mailto:info@loscaracoles.ch">info@loscaracoles.ch</a><br>
-            Mitglieder, Kasse, Buchhaltung: Andreas Stettler <a href="mailto:mitgliederverwaltung@loscaracoles.ch">mitgliederverwaltung@loscaracoles.ch</a><br>
-            Konzertorganisation: Daniel Zihler, Alfredo Palacios <a href="mailto:konzerte@loscaracoles.ch">konzerte@loscaracoles.ch</a><br>
-            </p>
-               
-
-
-                <form action="" method="post" data-form-title="CONTACT FORM">
-
-                    <input type="hidden" value="oREOq6MGQE87nW/6twLmvqfp2XuG4tz2zg+0LmOGGUZRpIU6TKnr1FFEuxOFksfREhl/TS9OrJjo/Y+U++thSxG6/IpKDGxqRJUS/nLXU7S6pEWcg6n8SvoCFefCOcuA" data-form-email="true" class="animated fadeInUp">
-
-                    <div class="row row-sm-offset">
-
-                        <div class="col-xs-12 col-md-4">
-                            <div class="form-group">
-                                <label class="form-control-label" for="form1-2h-name">Name<span class="form-asterisk">*</span></label>
-                                <input type="text" class="form-control" name="name" required="" data-form-field="Name" id="form1-2h-name">
-                            </div>
-                        </div>
-
-                        <div class="col-xs-12 col-md-4">
-                            <div class="form-group">
-                                <label class="form-control-label" for="form1-2h-email">Email<span class="form-asterisk">*</span></label>
-                                <input type="email" class="form-control" name="email" required="" data-form-field="Email" id="form1-2h-email">
-                            </div>
-                        </div>
-
-                        <div class="col-xs-12 col-md-4">
-                            <div class="form-group">
-                                <label class="form-control-label" for="form1-2h-phone">Phone</label>
-                                <input type="tel" class="form-control" name="phone" data-form-field="Phone" id="form1-2h-phone">
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div class="form-group">
-                        <label class="form-control-label" for="form1-2h-message">Message</label>
-                        <textarea class="form-control" name="message" rows="7" data-form-field="Message" id="form1-2h-message"></textarea>
-                    </div>
-
-                    <div><button type="button" class="btn btn-primary">Senden</button></div>
-
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
+<?php perch_content('kontakt') ?>
 <?php include('include/footer.html'); ?>
 
 
---
+++ reservationen.php
@@ -0,0 +1,10 @@
+<?php include('include/header.html'); ?>
+<?php include('include/navigation.php'); ?>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-8 mx-auto py-5">
+			 <?php perch_content('reservationen');	?>  
+		</div>
+	</div>
+</div>
+<?php include('include/footer.html'); ?>
