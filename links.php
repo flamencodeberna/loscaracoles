452afa7afa38bc12d11520952801999342952363
olijaun <olijaun@yahoo.com>
2018-01-07 14:09:03 +0100

    added links page

---
+++ links.php
@@ -0,0 +1,238 @@
+<?php include('include/header.html'); ?>
+<?php include('include/navigation.php'); ?>
+
+    <section class="py-5">
+        <div class="container">
+            <h1>Einige nützliche Links</h1>
+
+            <p>Die Liste wird laufend ergänzt! - <a
+                        href="mailto:info@loscaracoles.ch?subject=Link%20auf%20unsere%20Seite">Melden
+                    Sie uns Ihre Seite für einen Link</a></p>
+
+            <table border="0" cellpadding="0" cellspacing="2" bordercolor="#FFFF99" style="width:96%;margin-left:16px;">
+                <tr>
+                    <td colspan="3" style="background-color:#CCC;padding:3px;">Gitarren-Super-Estrellas</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><a href="http://www.pacodelucia.org" target="_blank">Paco de Luc&iacute;a</a></td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><a href="http://www.tomatito.com" target="_blank">Tomatito</a></td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><a href="http://www.vicenteamigo.com/" target="_blank">Vicente Amigo</a></td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="background-color:#CCC;padding:3px;">Flamenco allgemein</td>
+                </tr>
+                <tr>
+                    <td width="1%" align="center">&nbsp;</td>
+                    <td width="46%"><a href="http://www.anda.de" target="_blank">Deutsche Flamenco-Zeitschrift Anda</a>
+                    </td>
+                    <td width="53%">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td width="1%" align="center">&nbsp;</td>
+                    <td width="46%"><a href="http://www.juntadeandalucia.es/culturaydeporte/centroandaluzflamenco/"
+                                       target="_blank">Centro Andaluz de Flamenco in Jerez</a></td>
+                    <td width="53%">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td width="1%" align="center">&nbsp;</td>
+                    <td width="46%"><a href="http://www.deflamenco.com" target="_blank">deflamenco.com</a></td>
+                    <td width="53%">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td width="1%" align="center">&nbsp;</td>
+                    <td width="46%"><a href="http://www.flamenco-seiten.de/" target="_blank">Die deutschen
+                            Flamenco-Seiten</a></td>
+                    <td width="53%">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td align="center" width="1%">&nbsp;</td>
+                    <td width="46%"><a href="http://www.horizonteflamenco.com" target="_blank">Horizonte Flamenco</a>
+                    </td>
+                    <td width="53%">&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><a href="http://www.flamencofestival.com" target="_blank">Berliner Flamenco-Festival</a></td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="background-color:#CCC;padding:3px;">Flamenco-Schulen Schweiz</td>
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td><span class="fliesstext11print"><a
+                                    href="http://www.alfredopalacios.ch/flamenco-gitarrenunterricht-online.htm"
+                                    target="_blank">www.alfredopalacios.ch</a></span></td>
+                    <td>Alfredo Palacios, Bern</td>
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td><a href="http://www.flamencobern.ch" target="_blank">www.flamencobern.ch</a></td>
+                    <td>Flamenco Tanzstudio Susana, Susanna Eckert Bern</td>
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td><a href="http://www.flamencobiel.ch" target="_blank">www.flamencobiel.ch</a></td>
+                    <td>Corinne Togul, Biel</td>
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td><a href="http://www.flamencokellerbern.ch" target="_blank">www.flamencokellerbern.ch</a></td>
+                    <td>Franziska Badertscher, Bern</td>
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td><a href="http://www.flamencotanz-la-ursula.ch/"
+                           target="_blank">www.flamencotanz-la-ursula.ch</a>
+                    </td>
+                    <td>Ursula Nett, Bern</td>
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td><a href="http://www.flamenco-manuela.ch" target="_blank">www.flamenco-manuela.ch</a></td>
+                    <td>Manuela K&auml;mpf</td>
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td><a href="http://www.flamenco-rosa.ch" target="_blank">www.flamenco-rosa.ch</a></td>
+                    <td>Tina Wyss und Michael Abramski, Basel</td>
+                </tr>
+                <tr>
+                    <td align="center" class="linkseite_abschnitt_titel">&nbsp;</td>
+                    <td><a href="http://www.juliastucki.ch" target="_blank">www.juliastucki.ch</a></td>
+                    <td>Julia Stucki - Flamenco, Tango, Jazz</td>
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td><a href="http://www.pasion-flamenco.ch" target="_blank">www.pasion-flamenco.ch</a></td>
+                    <td>Silvia Crego, Basel</td>
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td><a href="http://www.poralegria.ch" target="_blank">www.poralegria.ch</a></td>
+                    <td>Alicia L&oacute;pez, Bern</td>
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td><a href="http://www.taller-flamenco.ch" target="_blank">www.taller-flamenco.ch</a></td>
+                    <td>Teresa Palacios, Bern</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><a href="http://www.antonioflores.net/" target="_blank">www.antonioflores.net</a></td>
+                    <td>Antonio Flores</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><a href="http://www.flamenco-sh.ch" target="_blank">www.flamenco-sh.ch</a></td>
+                    <td>Die Amigos aus Schaffhausen</td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="background-color:#CCC;padding:3px;">Fanclubs</td>
+                </tr>
+                <tr>
+                    <td width="1%" align="center">&nbsp;</td>
+                    <td width="46%"><a href="http://www.flamencovivo.ch" target="_blank">www.flamencovivo.ch</a></td>
+                    <td width="53%">Der aktive Berner Fan Club</td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="background-color:#CCC;padding:3px;">Flamenco Musik- und Tanzgruppen</td>
+                </tr>
+                <tr>
+                    <td width="1%" align="center">&nbsp;</td>
+                    <td width="46%"><a href="http://www.contrabando.ch" target="_blank">www.contrabando.ch</a></td>
+                    <td width="53%">Anet Fr&ouml;lichers fr&ouml;hliche Schmugglerinnen</td>
+                </tr>
+                <tr>
+                    <td width="1%" align="center">&nbsp;</td>
+                    <td width="46%"><a href="http://www.flamenco-show.ch" target="_blank">www.flamenco-show.ch</a></td>
+                    <td width="53%">Die &quot;Verzauberten&quot; aus Z&uuml;rich</td>
+                </tr>
+                <tr>
+                    <td align="center" class="linkseite_abschnitt_titel" width="1%">&nbsp;</td>
+                    <td width="46%"><a href="http://www.madruga-flamenca.de" target="_blank">www.madruga-flamenca.de</a>
+                    </td>
+                    <td width="53%">Morgend&auml;mmerung in Freiburg</td>
+                </tr>
+                <tr>
+                    <td align="center" class="linkseite_abschnitt_titel">&nbsp;</td>
+                    <td><a href="http://www.nickperrin.ch" target="_blank">www.nickperrin.ch</a></td>
+                    <td>Nick Perrin Flamenco Jazz Quartett</td>
+                </tr>
+                <tr>
+                    <td align="center" class="linkseite_abschnitt_titel">&nbsp;</td>
+                    <td><a href="http://www.coro-rociero.ch" target="_blank">www.coro-rociero.ch</a></td>
+                    <td>Romer&iacute;a de Roc&iacute;o ... in Luzern!</td>
+                </tr>
+                <tr>
+                    <td align="center" class="linkseite_abschnitt_titel" width="1%">&nbsp;</td>
+                    <td width="46%"><a href="http://www.flamencos-enroute.com"
+                                       target="_blank">www.flamencos-enroute.com</a>
+                    </td>
+                    <td width="53%">Brigitta Luisas Truppe</td>
+                </tr>
+                <tr>
+                    <td align="center" class="linkseite_abschnitt_titel">&nbsp;</td>
+                    <td><a href="http://www.cuadro-flamenco.ch" target="_blank">www.cuadro-flamenco.ch</a></td>
+                    <td>Rafael Segura</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><a href="http://www.lacuadrilla.ch" target="_blank">www.lacuadrilla.ch</a></td>
+                    <td>Cristina de Jerez und ihr Flamenco-Rhythmus-Chor</td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                    <td><a href="http://www.flamenco-carmendetorres.com"
+                           target="_blank">www.flamenco-carmendetorres.com</a>
+                    </td>
+                    <td>Carmen de Torres</td>
+                </tr>
+                <tr>
+                    <td colspan="3" style="background-color:#CCC;padding:3px;"><a
+                                href="http://www.flamenco-leonormoro.ch"></a>Kommerzielle Links
+                    </td>
+                </tr>
+                <tr>
+                    <td align="center" class="fliesstext10">&nbsp;</td>
+                    <td><a href="http://www.websaiten.ch" target="_blank">www.websaiten.ch</a></td>
+                    <td>Online Shop f&uuml;r Musiksaiten</td>
+                </tr>
+                <tr>
+                    <td align="center" class="fliesstext10">&nbsp;</td>
+                    <td><a href="http://www.mundo-flamenco.ch" target="_blank">www.mundo-flamenco.ch</a></td>
+                    <td>Werni W&auml;ckerlig &amp; Co., Z&uuml;rich</td>
+                </tr>
+                <tr>
+                    <td align="center" class="fliesstext10">&nbsp;</td>
+                    <td><a href="http://www.arte-flamenco.ch" target="_blank">www.arte-flamenco.ch</a></td>
+                    <td>Pedro Navarro &amp; Co., Basel</td>
+                </tr>
+                <tr>
+                    <td align="center" class="fliesstext10">&nbsp;</td>
+                    <td><a href="http://www.flamencoexport.com" target="_blank">www.flamencoexport.com</a></td>
+                    <td>Die exportieren alles</td>
+                </tr>
+                <tr>
+                    <td align="center" class="fliesstext10">&nbsp;</td>
+                    <td><a href="http://www.flamenco-events.com" target="_blank">www.flamenco-events.com</a></td>
+                    <td>Die Event-Organisatoren aus dem Waadtland</td>
+                </tr>
+            </table>
+
+        </div>
+    </section>
+
+    <!-- InstanceEndEditable -->
+
+<?php include('include/footer.html'); ?>
