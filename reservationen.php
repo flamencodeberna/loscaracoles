353bec70ac53d390327c525362453f23e3e8b7f6
olijaun <olijaun@yahoo.com>
2018-05-10 15:16:34 +0200

    added comment

--- reservationen.php
+++ reservationen.php
@@ -5,6 +5,7 @@
 		<div class="col-lg-8 mx-auto py-5">
 			<h1>Livio Gianola</h1>
 			<p>Sa 09. Juni 2018, Lorrainestrasse 4, 3013 Bern</p>
+            <p>ACHTUNG: Reservation ist bis am 18.05.2018 nur für Caracoes-Mitglieder möglich. Anschliessend ist es für alle offen.</p>
 			 <?php perch_content('reservationen');	?>  
 		</div>
 	</div>
