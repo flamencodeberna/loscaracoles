6ed9040cda4c1614649929857b8a17712a2c9cb6
olijaun <olijaun@yahoo.com>
2018-06-25 16:35:35 +0200

    changed name for reservation

--- reservationen.php
+++ reservationen.php
@@ -3,9 +3,9 @@
 <div class="container">
 	<div class="row">
 		<div class="col-lg-8 mx-auto py-5">
-			<h1>Livio Gianola</h1>
-			<p>Sa 09. Juni 2018, Lorrainestrasse 4, 3013 Bern</p>
-            <p>ACHTUNG: Reservation ist bis am 18.05.2018 nur für Caracoes-Mitglieder möglich. Anschliessend ist es für alle offen.</p>
+			<h1>Manuel Montero</h1>
+			<p>Fr 14. September 2018, Lorrainestrasse 4, 3013 Bern</p>
+            <p>ACHTUNG: Reservation ist bis am 18.07.2018 nur für Caracoes-Mitglieder möglich. Anschliessend ist es für alle offen.</p>
 			 <?php perch_content('reservationen');	?>  
 		</div>
 	</div>
