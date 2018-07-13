1a2f0e1219bd0b62552a3913b5a3d320862fc87e
olijaun <olijaun@yahoo.com>
2018-07-13 14:54:52 +0200

    fixed location

--- reservationen.php
+++ reservationen.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="col-lg-8 mx-auto py-5">
 			<h1>Manuel Montero</h1>
-			<p>Fr 14. September 2018, Lorrainestrasse 4, 3013 Bern</p>
+			<p>Fr 14. September 2018, Zieglerstrasse 26, 3007 Bern</p>
             <p>ACHTUNG: Reservation ist bis am 18.07.2018 nur für Caracoes-Mitglieder möglich. Anschliessend ist es für alle offen.</p>
 			 <?php perch_content('reservationen');	?>  
 		</div>
