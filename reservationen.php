9b3c235771f95d935f67d604bac9ccd977640f82
olijaun <olijaun@yahoo.com>
2018-09-02 15:21:33 +0200

    bold

--- reservationen.php
+++ reservationen.php
@@ -4,7 +4,7 @@
 	<div class="row">
 		<div class="col-lg-8 mx-auto py-5">
 			<h1>Reservationen</h1>
-			<p>Bitte versichern Sie sich, das Sie die korrekte Veranstaltung auswählen.</p>
+			<p><b>Bitte versichern Sie sich, das Sie die korrekte Veranstaltung auswählen.</b></p>
 			 <?php perch_content('reservationen');	?>  
 		</div>
 	</div>
